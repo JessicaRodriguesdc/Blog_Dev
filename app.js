90789575a557d44e8c141e2bd55f091d73633e2a
JessicaRodriguesdc <jessicarodridc@gmail.com>
2019-12-23 20:01:04 -0300

    Iniciando o projeto blog em node

---
+++ app.js
@@ -0,0 +1,18 @@
+//Carregando Modulos
+
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require("body-parser")
+const app = express()
+//const mongoose = require("mongoose")
+
+//Configuracoes
+    //Body
+
+//Rotas
+
+//Outras
+const PORT = 8083
+app.listen(PORT,()=>{
+    console.log("Servidor rodando!")
+}) 
