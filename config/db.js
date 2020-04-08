926948c69acd217e85cffa8f54be451674d6939a
Jéssica Rodrigues da Costa <jessicarodridc@gmail.com>
2020-04-08 15:26:55 -0300

    corrigindo conexao e pagina de editar postagens

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://jessica:blogapp6@blogapp-prod-reh51.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://jessica:blogapp6@blogapp-prod-reh51.mongodb.net/blogapp?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/blogapp"} 
 }
