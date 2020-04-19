a01002e2b87920e8db9c60fcba5e91912cde0ffa
Jéssica Rodrigues da Costa <jessicarodridc@gmail.com>
2020-04-19 19:26:47 -0300

    configurando o nodemon e o ambiente de producao e desenvolvimento

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 if(process.env.NODE_ENV == "production"){
+    //producao
     module.exports = {mongoURI: "mongodb+srv://jessica:blogapp6@blogapp-prod-reh51.mongodb.net/blogapp?retryWrites=true&w=majority"}
 }else{
+    //desenvolvimento
     module.exports = {mongoURI: "mongodb://localhost/blogapp"} 
 }
