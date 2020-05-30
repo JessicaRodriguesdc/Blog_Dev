42463d891b3e20dc22f7edb62659e0ce5f584c4b
Jéssica Rodrigues da Costa <jessicarodridc@gmail.com>
2020-05-30 17:25:03 -0300

    outras configuracoes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 if(process.env.NODE_ENV == "production"){
     //producao
     module.exports = {mongoURI: "mongodb+srv://jessica:blogapp6@blogapp-prod-reh51.mongodb.net/blogapp?retryWrites=true&w=majority"}
+    console.log("Producao");
 }else{
     //desenvolvimento
     module.exports = {mongoURI: "mongodb://localhost/blogapp"} 
+    console.log("Desenvolvimento");
 }
